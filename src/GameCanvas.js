50a1c18a72ab70863efdc69e81228ac4073c6f1d
Tim Dill <brodt7258@gmail.com>
2020-05-27 17:13:18 -0400

    setup game update interval

--- src/GameCanvas.js
+++ src/GameCanvas.js
@@ -1,82 +1,53 @@
-import React, { useRef, useEffect, useState, useLayoutEffect, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import useBufferGrid from './useBufferGrid';
+import useDoubleBuffer from './useDoubleBuffer';
+import useCanvas from './useCanvas';
 
 const GameCanvas = () => {
   const [generation, setGeneration] = useState(0);
-
-  const [cellSize, setCellSize] = useState(0);
   const [cellQuantity] = useState(20);
+  const [running, setRunning] = useState(null);
 
-  const bufferA = useBufferGrid(cellQuantity);
-  const bufferB = useBufferGrid(cellQuantity);
-
-  const onBuffer = useCallback(() => {
-    return generation % 2 === 0 ? bufferA : bufferB;
-  }, [generation, bufferA, bufferB]);
-
-  const offBuffer = useCallback(() => {
-    return generation % 2 === 1 ? bufferA : bufferB;
-  }, [generation, bufferA, bufferB]);
-
-  const canvasRef = useRef(null);
-  const containerRef = useRef(null);
-  useLayoutEffect(() => {
-    const { height, width } = containerRef.current.getBoundingClientRect();
-    canvasRef.current.height = height;
-    canvasRef.current.width = width;
-    setCellSize(width / cellQuantity);
-  }, [cellQuantity]);
+  const { currBuffer, updateNextBuffer } = useDoubleBuffer(generation, cellQuantity);
+  const [canvasRef, containerRef, drawState, mapPixelToCell] = useCanvas(cellQuantity);
 
   useEffect(() => {
-    drawState(canvasRef.current, onBuffer().grid, cellSize);
-  }, [cellSize, onBuffer]);
-
-  useEffect(() => {
-    offBuffer().computeNext(onBuffer().grid);
-  }, [generation, onBuffer, offBuffer]);
+    drawState(currBuffer.grid);
+  }, [drawState, currBuffer]);
 
   const incrementGen = () => {
-    const nextGen = generation + 1;
-    setGeneration(nextGen);
+    setGeneration((prev) => prev + 1);
   };
 
   const handleCanvasClick = (e) => {
-    const { top, left } = canvasRef.current.getBoundingClientRect();
+    const [row, col] = mapPixelToCell(e.clientX, e.clientY);
+    currBuffer.grid[row][col] = !currBuffer.grid[row][col];
+    drawState(currBuffer.grid);
+    updateNextBuffer();
+  };
 
-    const row = Math.floor((e.clientY - top) / cellSize);
-    const col = Math.floor((e.clientX - left) / cellSize);
-
-    onBuffer().grid[row][col] = !onBuffer().grid[row][col];
-    drawState(canvasRef.current, onBuffer().grid, cellSize);
-    offBuffer().computeNext(onBuffer().grid);
+  const toggleRunning = () => {
+    if (running) {
+      clearInterval(running);
+      setRunning(null);
+    } else {
+      const runInterval = setInterval(incrementGen, 500);
+      setRunning(runInterval);
+    }
   };
 
   return (
     <>
       <div ref={containerRef} className="canvas-container">
-        <canvas ref={canvasRef} width={200} height={200} onClick={handleCanvasClick} className="canvas" />
+        <canvas ref={canvasRef} onClick={handleCanvasClick} className="canvas" />
       </div>
       <div className="controls">
         <p>{generation}</p>
         <button type="button" onClick={incrementGen}>step</button>
+        <button type="button" onClick={toggleRunning}>{running ? 'pause' : 'play'}</button>
       </div>
     </>
   );
-};
-
-const drawState = (canvas, gameBuffer, cellSize) => {
-  const ctx = canvas.getContext('2d');
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = 'orange';
-  gameBuffer.forEach((row, y) => {
-    row.forEach((cell, x) => {
-      if (cell) {
-        ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
-      }
-    });
-  });
 };
 
 export default GameCanvas;
